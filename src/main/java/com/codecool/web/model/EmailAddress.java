8304bcaae6b61ad560ff8d3d791811de0335c07d
Gergő Papp <imgergopapp@gmail.com>
2019-03-15 15:35:26 +0100

    Added package

---
+++ src/main/java/com/codecool/web/model/EmailAddress.java
@@ -0,0 +1,12 @@
+package com.codecool.web.model;
+
+import java.util.List;
+
+public class EmailAddress {
+
+    private static List<String> emailAddresses;
+
+    public static List<String> getEmailAddresses() {
+        return emailAddresses;
+    }
+}
