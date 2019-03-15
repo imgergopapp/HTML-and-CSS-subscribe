846bb9664a45fe2155bdf925aed8af0d6af84a15
Gergő Papp <imgergopapp@gmail.com>
2019-03-15 15:36:33 +0100

    Added Servlet

--- src/main/java/com/codecool/web/servlet/SubscribeServlet.java
+++ src/main/java/com/codecool/web/servlet/SubscribeServlet.java
@@ -1,7 +1,28 @@
 package com.codecool.web.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/subscribe")
-public class SubscribeServlet {
+public class SubscribeServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String email = request.getParameter("email");
+        request.setAttribute("emailAddress",email);
+        request.getRequestDispatcher("index.jsp").include(request, response);
+
+    }
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+
+        }
 }
